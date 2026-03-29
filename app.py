dcfb7f0bfa72559fab9ef0f7d75209fbf1ea5b53
Finian Doonan <162761274+fin81985@users.noreply.github.com>
2026-03-29 12:40:20 +0100

    app update

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+
 from models import db, Expense
 from flask_cors import CORS
 
@@ -22,4 +23,52 @@ def home():
 if __name__ == '__main__':
     app.run(debug=True)
 
+@app.route('/expenses', methods=['POST'])
+def add_expense():
+    data = request.json
 
+    new_expense = Expense(
+        amount=data['amount'],
+        category=data['category'],
+        description=data.get('description', ''),
+        date=data.get('date', '')
+    )
+
+    db.session.add(new_expense)
+    db.session.commit()
+
+    return jsonify(new_expense.to_dict()), 201
+
+@app.route('/expenses', methods=['GET'])
+def get_expenses():
+    expenses = Expense.query.all()
+    return jsonify([e.to_dict() for e in expenses])
+
+@app.route('/expenses/<int:id>', methods=['GET'])
+def get_expense(id):
+    expense = Expense.query.get_or_404(id)
+    return jsonify(expense.to_dict())
+
+@app.route('/expenses/<int:id>', methods=['PUT'])
+def update_expense(id):
+    expense = Expense.query.get_or_404(id)
+    data = request.json
+
+    expense.amount = data.get('amount', expense.amount)
+    expense.category = data.get('category', expense.category)
+    expense.description = data.get('description', expense.description)
+    expense.date = data.get('date', expense.date)
+
+    db.session.commit()
+
+    return jsonify(expense.to_dict())
+
+
+@app.route('/expenses/<int:id>', methods=['DELETE'])
+def delete_expense(id):
+    expense = Expense.query.get_or_404(id)
+
+    db.session.delete(expense)
+    db.session.commit()
+
+    return jsonify({"message": "Deleted successfully"})
